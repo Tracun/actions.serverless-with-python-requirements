2ba7bdb7771af3f1164dc3e222205d27c206253d
Lucas Waiteman Bastos <lucas060994@gmail.com>
2021-11-24 15:41:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function runServerlessDeploy() {
     `if [ ${process.env.AWS_ACCESS_KEY_ID} ] && [ ${process.env.AWS_SECRET_ACCESS_KEY} ]; then
       sls config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} ${ARGS}
     fi`,
-    `sls update ${ARGS}`
+    `sls deploy -u ${ARGS}`
   )
 }
 
